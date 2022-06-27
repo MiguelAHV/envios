63ac0640a4b7448d8f0b957a3ad74b172715c466
Miguel Angel Herrera <ambientmiguel@gmail.com>
2022-06-27 17:22:09 -0500

    27/06

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,47 @@
-const { src, dest, watch, series } = require('gulp');
+const { src, dest, watch, series, parallel } = require("gulp");
 
 // CSS y SASS
-const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-const cssnano = require('cssnano');
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
 
-// Imagenes
-const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
-const avif = require('gulp-avif');
+
+
 
 function css( done ) {
-    src('src/scss/app.scss')
-        .pipe( sourcemaps.init() )
+    // compilar
+    // pasos: 1- identificar archivo, 2- compilarla, 3- guardar el css
+
+    src("src/scss/app.scss")
         .pipe( sass() )
-       /*  .pipe( postcss([ autoprefixer(), cssnano() ]) ) */
-        .pipe( postcss([ autoprefixer() ]) )
-        .pipe( sourcemaps.write('.'))
-        .pipe( dest('build/css') )
+        .pipe( postcss( [autoprefixer(), ] ) )
+        .pipe( dest("build/css") )
+
 
     done();
 }
 
-function imagenes() {
-    return src('src/img/**/*')
-        .pipe( imagemin({ optimizationLevel: 3 }) )
-        .pipe( dest('build/img') )
-}
+function imagenes ( done ) {
+    src("src/img/**/*")
+        .pipe( dest("build/img") );
 
-function versionWebp() {
-    const opciones = {
-        quality: 50
-    }
-    return src('src/img/**/*.{png,jpg}')
-        .pipe( webp( opciones ) )
-        .pipe( dest('build/img') )
-}
-function versionAvif() {
-    const opciones = {
-        quality: 50
-    }
-    return src('src/img/**/*.{png,jpg}')
-        .pipe( avif( opciones ) )
-        .pipe( dest('build/img'))
+    done();
 }
 
 function dev() {
-    watch( 'src/scss/**/*.scss', css );
-    watch( 'src/img/**/*', imagenes );
+    
+    watch("src/scss/**/*.scss", css);
+    watch("src/img/**/*", imagenes);
+    
 }
 
 
+
 exports.css = css;
-exports.dev = dev;
+exports.dev = dev; 
 exports.imagenes = imagenes;
-exports.versionWebp = versionWebp;
-exports.versionAvif = versionAvif;
-exports.default = series( imagenes, versionWebp, versionAvif, css, dev  );
+exports.default = series( imagenes, css, dev ) ;
+
+// series - se inicia un tarea ya hasta que finalza sigue con la siguiente
+
+// parallel - todas las tareas inician al mismo tiempo
